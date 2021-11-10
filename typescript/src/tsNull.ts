9fb3aeb75fe3c43599bb078387da0dd819a7e070
Christiano0407 <placebo0704@gmail.com>
2021-11-10 15:41:24 -0600

    Object Ts

--- typescript/src/tsNull.ts
+++ typescript/src/tsNull.ts
@@ -1,3 +1,6 @@
 // Null and Undefined
 //> ts --watch src/main.ts --strictNullChecks
 //>> Nos mostrará todos los errores dentro del proyecto>
+//>>> Recuerda editar:
+// "Strict": true => false / false a true
+// "strictNullChecks": false => true / true a false
---
+++ typescript/src/tsObject.ts
@@ -0,0 +1,2 @@
+// Object es el tipo de dato que representa un valor no primitivo>>
+// Es el tipo para variable que no sea "number" / "dtring" / "Boolean" / "null" / "undefined">>>
